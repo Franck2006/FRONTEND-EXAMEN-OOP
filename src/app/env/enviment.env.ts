cd6846e5083ad71ed5287b74d2ba036bd7580593
Amani Franck <amanifranck2005@gmail.com>
2026-04-04 08:23:53 +0200

    init_url

--- src/app/env/enviment.env.ts
+++ src/app/env/enviment.env.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: false,
     supabaseUrl: 'https://your-supabase-url.supabase.co',
     supabaseKey: 'your-supabase-key',
-    API: 'http://localhost:3000'
+    // API: 'http://localhost:3000'
+    API: 'https://backend-supabase-login.onrender.com'
 };
