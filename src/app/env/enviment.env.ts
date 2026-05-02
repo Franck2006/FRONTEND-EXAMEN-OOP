152a4b30b7573e5781f25b63bc027b5a43d9ef71
Amani Franck <amanifranck2005@gmail.com>
2026-05-02 15:07:33 +0200

    url

--- src/app/env/enviment.env.ts
+++ src/app/env/enviment.env.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    production: false,
-    supabaseUrl: 'https://your-supabase-url.supabase.co',
-    supabaseKey: 'your-supabase-key',
-    API: 'http://localhost:3000' 
-    // API:  "https://backend-examen-oop.onrender.com"
+  production: false,
+  supabaseUrl: 'https://your-supabase-url.supabase.co',
+  supabaseKey: 'your-supabase-key',
+  // API: 'http://localhost:3000'
+  API: 'https://backend-examen-oop.onrender.com',
 };
