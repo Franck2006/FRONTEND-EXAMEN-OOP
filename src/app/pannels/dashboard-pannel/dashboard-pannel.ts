823c6be464e67fc0a16708e03223ff0bd120f60c
Amani Franck <amanifranck2005@gmail.com>
2026-05-05 23:05:22 +0200

    feat: Update chart.js version and integrate ng2-charts for line chart visualization in dashboard panel; enhance HTML structure and styles

--- src/app/pannels/dashboard-pannel/dashboard-pannel.ts
+++ src/app/pannels/dashboard-pannel/dashboard-pannel.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, signal } from '@angular/core';
+import { NgChartsModule } from 'ng2-charts';
 
 @Component({
   selector: 'app-dashboard-pannel',
-  imports: [CommonModule],
+  imports: [CommonModule, NgChartsModule],
   templateUrl: './dashboard-pannel.html',
   styleUrl: './dashboard-pannel.css',
 })
@@ -13,6 +14,26 @@ export class DashboardPannel implements OnInit {
   ngOnInit(): void {
     this.getAllTheCount();
   }
+
+  lineChardData = {
+    labels: ['sun', 'mon', 'tues', 'wed', 'fri', 'sat'],
+    datasets: [
+      {
+        data: [43, 65, 65, 34, 43, 43, 43],
+        label: 'sales',
+        fill: 'origin',
+      },
+      {
+        data: [43, 45, 45, 34, 43, 43, 43],
+        label: 'height',
+        // fill: true,
+      },
+    ],
+  };
+
+  lineChartOption = {
+    reponsive: false,
+  };
 
   allNumbers = signal<any>({});
 
